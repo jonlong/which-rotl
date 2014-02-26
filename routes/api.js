cdcb9fe5cc7b5cd3cf2a6befb1518ee306332a84
Jon Long <jon@linesandwaves.com>
2014-02-26 12:54:44 -0500

    post/receive and db storage

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 var config = require('../config');
 var Entry = require('../models/entry');
 var desk = require('../lib/desk');
+var buildEntry = require('../lib/buildEntry');
 var logger = require('winston');
 
 
@@ -11,47 +12,25 @@ module.exports = function(app, express) {
 
   // New entry receive hook
   app.post('/entry/receive', basicAuth, function(req, res) {
+    var data = JSON.parse(req.body.data);
 
-    logger.info('WEBHOOK RECEIVE', req.body);
+    // Create a new entry for the db
+    var entry = buildEntry.db(data);
 
-    // Save the entry to the database
-    // var entry = {
-    //   topic: req.body.topic,
-    //   episode_number: req.body.episode_number,
-    //   time_start: req.body.time_start,
-    //   time_stop: req.body.time_stop,
-    //   author_name: req.body.author_name,
-    //   author_link: req.body.author_link
-    // };
-
-    // new Entry(entry).save();
+    // Save to db
+    new Entry(entry).save(function(err) {
+      if (err) {
+        logger.warn(err);
+      } else {
+        logger.info('record saved');
+      }
+    });
   });
 
   app.post('/entry/submit', basicAuth, function(req, res) {
 
     // Create the case object
-    var entry = {
-      subject: 'Some Subject',
-      priority: 5,
-      description: 'Some Description',
-      status: 'new',
-      type: 'email',
-      labels: ['Submission'],
-      custom_fields: {},
-      message: {
-        to: 'jon@linesandwaves.com',
-        direction: 'in',
-        status: 'received',
-        body: 'Some Body',
-        subject: 'Some Subject'
-      },
-      _links: {
-        customer: {
-          href: '',
-          'class': 'customer'
-        }
-      }
-    };
+    var entry = buildEntry.desk(req.body);
 
     // Get a customer record
     desk.findOrCreateCustomer(req.body, function(err, customerLink) {
