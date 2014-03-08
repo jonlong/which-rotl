e39a63dfbfe247e89893816efc98f58db0087977
Jon Long <jon@linesandwaves.com>
2014-03-08 15:51:24 -0500

    send episode list to views

--- routes/site.js
+++ routes/site.js
@@ -1,8 +1,20 @@
+var getEpisodeList = require('./middleware/getEpisodeList');
+
 module.exports = function(app) {
 
   // Index Page
   app.get('/', function(req, res) {
     res.render('index');
+  });
+
+  app.get('/entry', function(req, res) {
+    res.render('entry');
+  });
+
+  app.get('/submit', getEpisodeList, function(req, res) {
+    res.render('submit', {
+      episodes: req.episodes
+    });
   });
 
 };
