5242a51c5ca1cb0255454971e25d40c83b316b7b
Jon Long <jon@linesandwaves.com>
2014-03-08 15:50:56 -0500

    retrieve list of episodes via middleware

---
+++ routes/middleware/getEpisodeList.js
@@ -0,0 +1,25 @@
+var fs = require('fs');
+var config = require('../../config');
+var feed = require('../../lib/feed');
+var _ = require('lodash');
+var logger = require('winston');
+
+module.exports = function(req, res, next) {
+
+  feed.listEpisodes(function(err, episodes) {
+    if (err) {
+      logger.error(err);
+      res.json({
+        result: 'error',
+        msg: err
+      });
+    }
+
+    var titles = _.map(episodes, function(episode) {
+      return episode.title;
+    });
+
+    req.episodes = titles;
+    next();
+  });
+};
