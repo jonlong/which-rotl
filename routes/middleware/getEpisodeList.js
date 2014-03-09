90d8f97bb16dc68191132aeb125f40c29f555f78
Jon Long <jon@linesandwaves.com>
2014-03-09 12:28:35 -0400

    proper http status

--- routes/middleware/getEpisodeList.js
+++ routes/middleware/getEpisodeList.js
@@ -9,10 +9,7 @@ module.exports = function(req, res, next) {
   feed.listEpisodes(function(err, episodes) {
     if (err) {
       logger.error(err);
-      res.json({
-        result: 'error',
-        msg: err
-      });
+      res.send(500, err);
     }
 
     var titles = _.map(episodes, function(episode) {
