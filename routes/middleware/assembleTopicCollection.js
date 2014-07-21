67ca497de9291a7e7cc46a3866e6be8daccf6518
Jon Long <jon@linesandwaves.com>
2014-07-21 18:34:54 -0400

    add appearances and image src/alt to topic collections

--- routes/middleware/assembleTopicCollection.js
+++ routes/middleware/assembleTopicCollection.js
@@ -3,6 +3,7 @@ var config = require('../../config');
 var feed = require('../../lib/feed');
 var _ = require('lodash');
 var logger = require('winston');
+var cheerio = require('cheerio');
 
 module.exports = function(req, res, next) {
 
@@ -15,6 +16,7 @@ module.exports = function(req, res, next) {
   // make a data structure that looks like this:
   var topicCollection = {
     topic: res.topicEntries[0].topic_display,
+    appearances: 0,
     data: []
   };
 
@@ -22,12 +24,22 @@ module.exports = function(req, res, next) {
     var collection = {};
     collection.episode = res.episodeDetails[i];
 
+    // The "content" value is a big HTML string with a bunch of freeform metadata for each episode.
+    // This line extracts the image associated with each.
+    var $ = cheerio.load(collection.episode.content);
+    var image = $('img').first();
+    collection.episode.image = {
+      src: image.attr('src'),
+      alt: image.attr('alt')
+    };
+
     // Find all the topic instances that reference this episode
     var topics = _.filter(res.topicEntries, function(topic) {
       return topic.episode_number === collection.episode.episode_number;
     });
 
     collection.appearances = topics;
+    topicCollection.appearances += collection.appearances.length;
     topicCollection.data.push(collection);
   }
 
